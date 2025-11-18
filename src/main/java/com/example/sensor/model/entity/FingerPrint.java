d089544b88e0f86668a4a32a8a41b18f196627b6
jaime <109822463+RpC05@users.noreply.github.com.>
2025-11-18 00:32:06 -0500

    1hr depsues

--- src/main/java/com/example/sensor/model/entity/FingerPrint.java
+++ src/main/java/com/example/sensor/model/entity/FingerPrint.java
@@ -6,10 +6,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.hibernate.type.SqlTypes;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -26,16 +25,27 @@ public class FingerPrint {
     @Column(name="fprint_id_int", nullable = false, unique = true)
     private Integer fingerprintId;
 
-    @Column(name="fprint_name_vac", nullable = false)
-    private String name;
+    // Datos personales
+    @Column(name="fprint_nombres_vac", nullable = false)
+    private String nombres;
+
+    @Column(name="fprint_apellido_paterno_vac", nullable = false)
+    private String apellidoPaterno;
+
+    @Column(name="fprint_apellido_materno_vac")
+    private String apellidoMaterno;
+
+    @Column(name="fprint_fecha_nacimiento_dt")
+    private LocalDate fechaNacimiento;
+
+    @Column(name="fprint_tipo_documento_vac")
+    private String tipoDocumento;
+
+    @Column(name="fprint_nro_documento_vac", unique = true)
+    private String numeroDocumento;
 
     @Column(name="fprint_desc_vac")
-    @JdbcTypeCode(SqlTypes.LONGNVARCHAR)
     private String description;
-
-    @Column(name = "fprint_template_data_vac")
-    @JdbcTypeCode(SqlTypes.LONGNVARCHAR)
-    private String templateData;
 
     @CreationTimestamp
     @Column(name="fprint_cre_dt", updatable = false)
